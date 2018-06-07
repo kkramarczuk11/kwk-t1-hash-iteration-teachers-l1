9a9ae0f39750d21a03616c5323a11d4c0e713d6d
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-07 15:14:39 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,15 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids = {
+    Timmy: 9,
+    Sarah: 6,
+    Amanda: 27}
+    hash.each do
+      puts "Happy Birthday #{key}! You are now #{value} years old!"
 end
-
-
